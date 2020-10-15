7fac5844d7ea74faed51e5069ceadbbf76736653
DBAK <zhongjie007@yeah.net>
2020-10-15 15:08:04 +0800

    v1.x版本开发
    修正了__init__.py文件的问题，可以作为第三方包导入其他项目

--- __init__.py
+++ __init__.py
@@ -13,25 +13,3 @@
 # Licensed to GPL under a Contributor Agreement.
 #
 
-import sys
-
-#
-# Copy stuff from default context
-#
-
-__all__ = [x for x in dir(context._default_context) if not x.startswith('_')]
-globals().update((name, getattr(context._default_context, name)) for name in __all__)
-
-#
-# XXX These should not really be documented or public.
-#
-
-SUBDEBUG = 5
-SUBWARNING = 25
-
-#
-# Alias for main module -- will be reset by bootstrapping child processes
-#
-
-if '__main__' in sys.modules:
-    sys.modules['__parallel_line_process__'] = sys.modules['__main__']
